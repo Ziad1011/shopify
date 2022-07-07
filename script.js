8316844155d1a31f70a397b329f003e18636ab47
Ziad1011 <96190914+Ziad1011@users.noreply.github.com>
2022-07-07 16:08:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(".start").click(function(){
+      $(".sub-menu1").toggle();
+     
+    });
+    $(".sell").click(function(){
+        $(".sub-menu2").toggle();
+       
+      });
+      $(".market").click(function(){
+        $(".sub-menu3").toggle();
+       
+      });
+      $(".manage").click(function(){
+        $(".sub-menu4").toggle();
+       
+      });
+    
+  });
+  
+    
